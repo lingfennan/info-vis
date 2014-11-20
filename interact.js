bdc8928944ac255793361607e6d828522abc100c
Ruian Duan <duanruian@gmail.com>
2014-11-19 21:35:45 -0500

    The new data is kind of ported, with many bugs.

--- interact.js
+++ interact.js
@@ -6,6 +6,13 @@ var organizations = [];
 var globalDataset;
 var cluster = ["Type", "Orgs Involved"];
 
+// Use by timeline_init
+var domElement = "#timeline";
+var mysourceFile = "data.csv";
+var sourceFile = "philosophers.csv";
+
+
+
 function addOptions(select, data) {
 	select.selectAll("option").remove();
 	select.selectAll("option").data(data).enter().append("option").text(function(d) {
@@ -18,13 +25,16 @@ function addOptions(select, data) {
 
 function init() {
 	d3.csv(mysourceFile, function(dataset) {
-		globalDataset = dataset;
 		type.push("All Events");
 		organizations.push("All Organizations");
 		dataset.forEach(function(d) {
-			d.label = d["Title"];
+			d.title = d["Title"];  // Used for plotting dots.
 			d.start = d["Date"];
-			d.end = d["DateEnded"];
+            if (d["DateEnded"] == "") {
+                d.end = ""
+            } else {
+				d.end = d["DateEnded"]
+            }
 			if ((typeof d["Type"] != "undefined") &&
 				(d["Type"] != "")) {
 				type.push(d["Type"]);
@@ -38,6 +48,7 @@ function init() {
 		organizations = d3.set(organizations).values();
 		addOptions(d3.select("#showing"), type);
 		addOptions(d3.select("#cluster"), cluster);
+		globalDataset = dataset;
 		updateSelectedDataset();
 	});
 }
@@ -56,36 +67,28 @@ function updateSelectedDataset() {
 	var cluster_choice = d3.select("#cluster").node().value;
 	console.log(showing_choice);
 	console.log(cluster_choice);
-	var data = d3.nest()
+	// Set label
+	globalDataset.forEach(function(d) {
+		d.label = d[cluster_choice];
+	});
+	var selectedDataset = d3.nest()
 		.key(function(d) {
 			if (showing_choice == "All Events") {
-				return d[cluster_choice];
-			} else if (d[cluster_choice] == showing_choice) {
-				return d[cluster_choice];
+				return d.label;
+			} else if (d.label == showing_choice) {
+				return d.label;
 			}
 		})
 		.rollup(function(d) {
-			d.start = d["Date"];
-			d.end = d["DateEnded"];
+			return {"start": d3.min(d, function (g) { return g.start; }),
+			   	"end": d3.max(d, function(g) { return g.end; })};
 		})
 		.entries(globalDataset);
-
-}
-
-function updateSelected(id, value) {
-	// update cluster, affects options in showing
-	// update showing, doesn't affect options
-	if (id == "cluster") {
-		if (value == cluster[0]) {
-			addOptions(d3.select("#showing"), type);
-		} else {
-			addOptions(d3.select("#showing"), organizations); 
-		}
-		clusterSelection = value;
-	} else {
-		showingSelection = value;
-	}
-	updateSelectionDataset();
+	selectedDataset.forEach(function(d) {
+		d.label = d.key;
+		d.start = d.values.start;
+		d.end = d.values.end;
+	})
 	timeline(domElement)
 		.data(selectedDataset)
 	    .band("mainBand", 0.82)
@@ -97,4 +100,84 @@ function updateSelected(id, value) {
         .labels("naviBand")
         .brush("naviBand", ["mainBand"])
         .redraw();
+
+}
+
+
+function timelint_init() {
+    /*  You need a domElement, a sourceFile and a timeline.
+
+        The domElement will contain your timeline.
+        Use the CSS convention for identifying elements,
+        i.e. "div", "p", ".className", or "#id".
+
+        The sourceFile will contain your data.
+        If you prefer, you can also use tsv, xml, or json files
+        and the corresponding d3 functions for your data.
+
+
+        A timeline can have the following components:
+
+        .band(bandName, sizeFactor
+            bandName - string; the name of the band for references
+            sizeFactor - percentage; height of the band relation to the total height
+            Defines an area for timeline items.
+            A timeline must have at least one band.
+            Two bands are necessary, to change the selected time interval.
+            Three and Bands are allowed.
+
+        .xAxis(bandName)
+            bandName - string; the name of the band the xAxis will be attached to
+            Defines an xAxis for a band to show the range of the band.
+            This is optional, but highly recommended.
+
+        .labels(bandName)
+            bandName - string; the name of the band the labels will be attached to
+            Shows the start, length and end of the range of the band.
+            This is optional.
+
+        .tooltips(bandName)
+            bandName - string; the name of the band the labels will be attached to
+            Shows current start, length, and end of the selected interval of the band.
+            This is optional.
+
+        .brush(parentBand, targetBands]
+            parentBand - string; the band that the brush will be attached to
+            targetBands - array; the bands that are controlled by the brush
+            Controls the time interval of the targetBand.
+            Required, if you want to control/change the selected time interval
+            of one of the other bands.
+
+        .redraw()
+            Shows the initial view of the timeline.
+            This is required.
+
+        To make yourself familiar with these components try to
+        - comment out components and see what happens.
+        - change the size factors (second arguments) of the bands.
+        - rearrange the definitions of the components.
+    */
+
+    // Define domElement and sourceFile
+	
+//<script>
+//
+//    // Read in the data and construct the timeline
+//    d3.csv(sourceFile, function(dataset) {
+//        timeline(domElement)
+//            .data(dataset)
+//            .band("mainBand", 0.82)
+//            .band("naviBand", 0.08)
+//            .xAxis("mainBand")
+//            .tooltips("mainBand")
+//            .xAxis("naviBand")
+//            .labels("mainBand")
+//            .labels("naviBand")
+//            .brush("naviBand", ["mainBand"])
+//            .redraw();
+//
+//    });
+//
+//</script>
+
 }
